204fe5bbfcbd5a5968948baeb6a4a694257ab192
bautistafogliariniliceo1-ctrl <bautista.fogliarini.liceo1@gmail.com>
2025-11-06 00:01:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("Mi primera inteligencia artificial")
 st.write("Hola! Esta es mi primera app con Streamlit funcionando correctamente.")
+
+import talento_tech
+
+if __name__ == "__main__":
+    talento_tech.main()
